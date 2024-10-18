46a5ecf9cfb9e2036abe17fc86ff78ae15a8945d
thrrinesh <24bct155@kgcas.com>
2024-10-18 15:05:05 +0530

    Update jump_game.c

--- C/jump_game.c
+++ C/jump_game.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool canJump(int *nums, int numsSize)
-{
+bool canJump(int *nums, int numsSize) {
+    // If there is only one element, we are already at the end.
+    if (numsSize == 1) {
+        return true;
+    }
+
     int maxReach = 0;
 
-    for (int i = 0; i < numsSize; i++)
-    {
-        // If we've reached a position that is not reachable, return false
-        if (i > maxReach)
-        {
+    for (int i = 0; i < numsSize; i++) {
+        // If the current position is not reachable, return false
+        if (i > maxReach) {
             return false;
         }
 
@@ -17,8 +19,7 @@ bool canJump(int *nums, int numsSize)
         maxReach = (i + nums[i] > maxReach) ? (i + nums[i]) : maxReach;
 
         // If we can reach the end of the array, return true
-        if (maxReach >= numsSize - 1)
-        {
+        if (maxReach >= numsSize - 1) {
             return true;
         }
     }
@@ -26,17 +27,13 @@ bool canJump(int *nums, int numsSize)
     return false;
 }
 
-int main()
-{
+int main() {
     int nums[] = {2, 3, 1, 1, 4}; // Example input array
     int numsSize = sizeof(nums) / sizeof(nums[0]);
 
-    if (canJump(nums, numsSize))
-    {
+    if (canJump(nums, numsSize)) {
         printf("Yes, you can reach the end of the array!\n");
-    }
-    else
-    {
+    } else {
         printf("No, you cannot reach the end of the array.\n");
     }
 
